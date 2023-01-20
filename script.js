bb081c7bfe2210fda8cb630b0c97be83f62788a7
laurenalexander2 <a.lauren@wustl.edu>
2023-01-19 20:18:08 -0500

    3 charts

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
-const config = {
-  type: 'doughnut',
-  data: data,
-};
-const data = {
-  labels: [
-    'Red',
-    'Blue',
-    'Yellow'
-  ],
-  datasets: [{
-    label: 'My First Dataset',
-    data: [300, 50, 100],
-    backgroundColor: [
+const ctx = document.getElementById('myChart1');
+      
+      new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+        datasets: [{
+          data: [42, 58],
+          backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 205, 86)'
+          ],
+        
+      }],
+      options : {
+        tooltips: false,
+      }
+  }
+  });
+
+  const cty = document.getElementById('myChart2');
+      
+  new Chart(cty, {
+    type: 'doughnut',
+    data: {
+    datasets: [{
+      data: [42, 58],
+      backgroundColor: [
       'rgb(255, 99, 132)',
       'rgb(54, 162, 235)',
       'rgb(255, 205, 86)'
-    ],
-    hoverOffset: 4
-  }]
-};
+      ],
+    
+  }],
+  options : {
+    tooltips: false,
+  }
+}
+});
+
+const ctz = document.getElementById('myChart3');
+      
+  new Chart(ctz, {
+    type: 'doughnut',
+    data: {
+    datasets: [{
+      data: [42, 58],
+      backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)'
+      ],
+    
+  }],
+  options : {
+    tooltips: false,
+  }
+}
+});
